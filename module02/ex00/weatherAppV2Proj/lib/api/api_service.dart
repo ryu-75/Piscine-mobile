a04e465f0324fb5e3b578a474a873e9f42119038
sasha <sasha.lorion@gmail.com>
2024-05-28 14:44:24 +0200

    [ADD] - gitignore

--- module02/ex00/weatherAppV2Proj/lib/api/api_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class ApiService {
-  final String  apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
-
-  Future<http.Response> fetchWeatherData() async {
-    return await http.get(Uri.parse(apiUrl));
-  }
-}
